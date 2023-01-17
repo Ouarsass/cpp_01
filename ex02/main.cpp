f64200a7bfb4e943529f931f44cc3222732302f8
Mustapha <mustapha@mouarsas-MacBook-Pro.local>
2023-01-17 13:46:26 +0100

    added_in

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,5 +5,12 @@ int     main()
     std::string string = "HI THIS IS BRAIN";
     std::string *stringPTR = &string;
     std::string &stringREF = string;
-    std::cout << "The memory address of the string variable" << std::endl;
+    std::cout << "The memory address of the string variable" << &string << std::endl;
+    std::cout << "The memory address held by stringPTR" << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF" << &stringREF << std::endl;
+
+    std::cout << "The value of the string variable" << string << std::endl;
+    std::cout << "The value pointed to by stringPTR\t"<< *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF"<< stringREF << std::endl;
+    return 0;
 }
